061d44a4c6391b62ae3809bf0b613377f835c558
Amateratsu888 <vendell.cg@gmail.com>
2021-10-05 14:14:24 +0100

    checkpoint API

---
+++ src/components/TableList.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import TableRow from './TableRow'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+const TableList = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios(
+                'https://jsonplaceholder.typicode.com/users',
+            )
+            setData(response.data)
+
+        }
+        fetchData()
+
+    }, [])
+
+
+
+    console.log(data)
+    return (
+        <>
+            <table className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">city</th>
+                        <th scope="col">phone</th>
+                        <th scope="col">company's name</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                    {data.map((user, index) => { return <TableRow user={user} key={index} /> })}
+                </tbody>
+            </table>
+
+
+        </>
+    )
+}
+export default TableList
